a39dff0a001eb0756ae92d9992e6e1f7d8eabad1
StlZero <112057152+StlZero@users.noreply.github.com>
2023-11-19 20:59:29 +0700

    Renamed some button class names in HTML Minor changes to CSS
    Button-click function now return and update numbers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Variables
 
-const buttons = document.querySelectorAll('.box');
+const num_buttons = document.querySelectorAll('.box');
+
+const display_b = document.querySelector('.display-b');
 
 const display_a = document.querySelector('.display-a');
 
@@ -22,24 +24,44 @@ function operate(num1, operator, num2) {
     
 };
 
+// function getNum1(num) {
+//     return num;
+// };
 
 
-
-
-buttons.forEach(button => {
+num_buttons.forEach(button => {
     button.addEventListener('click', () => {
-        // Case that button's value is a number
-        if (display_a.textContent == 0) { // Might break later on
-            display_a.textContent = button.value;
-        }
-        else {
-            display_a.textContent += button.value;
-        }
+        num1 += button.value;
 
-        // Case that button's value is an operator
-        if (['*', '/', '+', '-'].includes(button.value)) {
-            display_a.textContent = button.value;
-        }
-
+        console.log(num1);
     });
 });
+
+
+
+
+
+// Needs rework
+// num_buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         let num1_dis = '';
+
+//         // Case that button's value is a number
+//         if (display_a.textContent == 0) { // Might break later on
+//             display_a.textContent = button.value;
+//             num1_dis = button.value;
+//         }
+//         else {
+//             display_a.textContent += button.value;
+//             num1_dis += button.value;
+//         }
+
+//         // Case that button's value is an operator
+//         if (['*', '/', '+', '-'].includes(button.value)) {
+//             display_a.textContent = button.value;
+
+//             console.log(num1_dis);
+//         }
+
+//     });
+// });
