7114fb4aa4a0c05c857a26e6644f3d1efdf9499a
StlZero <112057152+StlZero@users.noreply.github.com>
2023-12-08 15:31:11 +0700

    Fixed calculator operations and functions
    Added delete button's function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const num_buttons = document.querySelectorAll('.box');
 
+const dot_button = document.querySelector('.box-dot')
+
 const o_buttons = document.querySelectorAll('.box-o');
 
 const eq_button = document.querySelector('.box-eq');
@@ -9,6 +11,8 @@ const eq_button = document.querySelector('.box-eq');
 const display_b = document.querySelector('.display-b');
 
 const display_a = document.querySelector('.display-a');
+
+const delete_button = document.querySelector('.delete')
 
 const clear_button = document.querySelector('.clear')
 
@@ -21,20 +25,22 @@ let const_num2 = "";
 
 
 // Debounce variables
-let o_debounce = false; // Special case that operator acts as equal sign
+let e_debounce = false;
+
+let o_debounce = false; // Might now need this
 
 
 // Functions
 
 function operate(num1, operator, num2) {
     if (operator == '+') {
-        return parseInt(num1) + parseInt(num2);
+        return (parseInt(num1) + parseInt(num2)).toString();
     } else if (operator == '-') {
-        return parseInt(num1) - parseInt(num2);
+        return (parseInt(num1) - parseInt(num2)).toString();
     } else if (operator == '*') {
-        return parseInt(num1) * parseInt(num2);
+        return (parseInt(num1) * parseInt(num2)).toString();
     } else if (operator == '/') {
-        return parseInt(num1) / parseInt(num2);
+        return (parseInt(num1) / parseInt(num2)).toString();
     } else {
         return 'ERROR';
     }
@@ -44,18 +50,18 @@ function operate(num1, operator, num2) {
 
 num_buttons.forEach(button => {
     button.addEventListener('click', () => {
-        if (num1 == "0" && operator == "") { // Case that first number is being clicked
+        if (num1 == "0" && o_debounce == false) { // Case that first number is being clicked
             num1 = button.value;
             display_a.textContent = num1;
-        } else if (num1 != "0" && operator == "") { // Case that the first number is more than one digit
+        } else if (num1 != "0" && o_debounce == false) { // Case that the first number is more than one digit
             num1 += button.value;
             display_a.textContent = num1;
-        } else if ((operator != "" && num2 == "") || o_debounce == true) { // Case that second number is being clicked
+        } else if (num2 == "" && o_debounce == true) { // Case that second number is being clicked
             num2 = button.value;
             display_a.textContent = num2;
 
             document.querySelector('.btnSpecial')?.classList.remove('btnSpecial');
-        } else if ((operator != "" && num2 != "") || o_debounce == true) { // Case that the second number is more than one digit
+        } else if (num2 != "" && o_debounce == true) { // Case that the second number is more than one digit
             num2 += button.value;
             display_a.textContent = num2;
 
@@ -66,39 +72,69 @@ num_buttons.forEach(button => {
 
 
 
+delete_button.addEventListener('click', () => {
+    if (o_debounce == true && num2 == "") { // Case that a number is deleted after operator-operation
+        num1 = num1.slice(0, -1);
+        display_a.textContent = num1;
+
+        if (num1 == "") {
+            num1 = "0";
+            display_a.textContent = num1;
+        }
+    } else if (o_debounce == false) {
+        num1 = num1.slice(0, -1);
+        display_a.textContent = num1;
+
+        if (num1 == "") {
+            num1 = "0";
+            display_a.textContent = num1;
+        }
+    } else {
+        num2 = num2.slice(0, -1);
+        display_a.textContent = num2;
+        
+        if (num2 == "") {
+            num2 = "0";
+            display_a.textContent = num2;
+        }
+    } 
+
+});
+
+
+
 o_buttons.forEach(button => {
     button.addEventListener('click', () => {
-        if (num1 != "" && operator != "" && num2 != "") { // Case that operator button acts as equal sign
-            num1 = operate(num1, operator, num2);
-            display_a.textContent = num1;
 
-            num2 = "";
+        if (o_debounce == true) {
+            if (num2 == "") {
+                operator = button.value;
+            } else {
+                num1 = operate(num1, operator, num2).toString();
+                display_a.textContent = num1;
+    
+                num2 = "";
+            }  
+        } else {
             o_debounce = true;
-
-            operator = button.value;
-        } else { // Normal case
-            operator = button.value;
         }
+
+        operator = button.value;
     });
 });
 
 
 
 eq_button.addEventListener('click', () => {
-
-    if (num1 != "0" && operator == "" && num2 == "") {
+    if (o_debounce == true) {
+        num1 = operate(num1, operator, num2).toString();
         display_a.textContent = num1;
-    } else if (num1 != "" && operator != "" && num2 != "") {
-        num1 = operate(num1, operator, num2);
-        display_a.textContent = num1;
-    } else if (num != "" && operator != "" && num2 == "") {
-        display_a.textContent = num1;
+    
+        num2 = "";
+        o_debounce = false;
     } else {
-        console.log("NEW ERROR");
+        display_a.textContent = num1;
     }
-
-    num2 = "";
-    o_debounce = false;
     
     document.querySelector('.btnSpecial')?.classList.remove('btnSpecial');
 });
